567fb370eaf963f70dbfe2c2f26b72490bd45c40
Wulfsta <wulfstawulfsta@gmail.com>
2025-05-13 18:17:31 -0400

    add taplo for toml formatting

---
+++ lang/toml.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, ... }:
+{
+  plugins = {
+    lsp.servers = {
+      taplo = {
+        enable = true;
+        filetypes = [
+          "toml"
+        ];
+      };
+    };
+    conform-nvim.settings = {
+      formatters_by_ft = {
+        toml = [ "taplo" ];
+      };
+      formatters = {
+        taplo = { command = lib.getExe pkgs.taplo; };
+      };
+    };
+  };
+}
