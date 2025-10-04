1797bc19808ac6ed1831e54212c009ead4bce6ef
Wulfsta <wulfstawulfsta@gmail.com>
2025-10-04 16:07:04 -0400

    update rust

--- lang/rust.nix
+++ lang/rust.nix
@@ -1,14 +1,77 @@
 { lib, pkgs, ... }:
 {
   plugins = {
-    rustaceanvim.enable = true;
+    rustaceanvim = {
+      enable = true;
+      settings = {
+        dap = {
+          adapter = {
+            command = lib.getExe' pkgs.lldb "lldb-dap";
+            type = "executable";
+          };
+          autoloadConfigurations = true;
+        };
+
+        server = {
+          default_settings = {
+            rust-analyzer = {
+              cargo = {
+                buildScripts.enable = true;
+                features = "all";
+              };
+
+              diagnostics = {
+                enable = true;
+                styleLints.enable = true;
+              };
+
+              checkOnSave = true;
+              check = {
+                command = "clippy";
+                features = "all";
+              };
+
+              files = {
+                excludeDirs = [
+                  ".cargo"
+                  ".direnv"
+                  ".git"
+                  "node_modules"
+                  "target"
+                ];
+              };
+
+              inlayHints = {
+                bindingModeHints.enable = true;
+                closureStyle = "rust_analyzer";
+                closureReturnTypeHints.enable = "always";
+                discriminantHints.enable = "always";
+                expressionAdjustmentHints.enable = "always";
+                implicitDrops.enable = true;
+                lifetimeElisionHints.enable = "always";
+                rangeExclusiveHints.enable = true;
+              };
+
+              procMacro = {
+                enable = true;
+              };
+
+              rustc.source = "discover";
+            };
+          };
+        };
+        tools.enable_clippy = true;
+      };
+    };
     crates.enable = true;
     conform-nvim.settings = {
       formatters_by_ft = {
         rust = [ "rustfmt" ];
       };
       formatters = {
-        rustfmt = { command = lib.getExe pkgs.rustfmt; };
+        rustfmt = {
+          command = lib.getExe pkgs.rustfmt;
+        };
       };
     };
   };
